4e096c697e4a76be563ab5b754c92a9fd33b6c84
nadia <nadia.gonzalez@bbconsult.co.uk>
2024-10-21 13:31:18 -0400

    solution.py added

---
+++ solution.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def make_array_from_list(some_list):
+    pass
+
+
+def make_array_from_number(num):
+    pass
+
+
+class NumpyBasics:
+    def add_arrays(self, a, b):
+        pass
+
+    def add_array_number(self, a, num):
+        pass
+
+    def multiply_elementwise_arrays(self, a, b):
+        pass
+
+    def dot_product_arrays(self, a, b):
+        pass
+
+    def dot_1d_array_2d_array(self, a, m):
+        # consider the 2d array to be like a matrix
+        pass
